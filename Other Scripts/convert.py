f9c45f8115d0ae09af2e02b6f682c19f73bc02fc
Samuel Côté <samuel.cote13@usherbrooke.ca>
2025-02-19 18:51:14 -0500

    Polars is faster than Pandas

--- Other Scripts/convert.py
+++ Other Scripts/convert.py
@@ -1,4 +1,4 @@
-import pandas as pd
+import polars as pl
 import scipy as sp
 
 data = sp.io.loadmat("./emnist-byclass.mat")["dataset"][0][0]
@@ -7,16 +7,18 @@ training = data[0][0][0]
 test = data[1][0][0]
 conversion_table = data[2]
 
+
 # Association dictionary between EMNIST labels and ascii.
 conversion_dict = {}
 for i in conversion_table:
     conversion_dict[i[0]] = i[1]
 
 
-def to_csv(data):
-    df = {i: [] for i in range(28*28)}
+def to_dataframe(data):
+    df = {str(i): [] for i in range(28*28)}
     df["label"] = []
 
+    count = 0
     for image, label, writer in zip(*data):
         # Because the label is an array with one element.
         label = label[0]
@@ -26,15 +28,20 @@ def to_csv(data):
         image = image.reshape(28, 28).T.flatten()
 
         for i, e in enumerate(image):
-            df[i].append(e)
+            df[str(i)].append(e)
         df["label"].append(chr(ascii))
 
-    return pd.DataFrame(df)
+        count += 1
+        print(count)
+        # Limit number of elements in dataframe
+        # if count > 10000:
+        #     break
+
+    return pl.DataFrame(df)
 
 
-training = to_csv(training)
-test = to_csv(test)
+training = to_dataframe(training)
+training.write_csv("training.csv")
 
-
-training.to_csv("training.csv", index=False)
-test.to_csv("test.csv", index=False)
+# test = to_dataframe(test)
+# test.write_csv("test.csv")
