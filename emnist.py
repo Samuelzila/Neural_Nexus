34ce5156e979375196f675380dae8bd36bb25f3c
= <samuel.cote13@usherbrooke.ca>
2025-02-24 21:08:29 -0500

    WIP need to handle iteration

--- emnist.py
+++ emnist.py
@@ -1,4 +1,6 @@
 import polars as pl
+import numpy as np
+import itertools
 
 
 def training():
@@ -19,7 +21,7 @@ def test():
     return load_from_csv("test")
 
 
-def training_batched(batch_size=100):
+def training_batched(batch_size=50000):
     """
     The batch size is an int representing the size of each batch.
 
@@ -30,7 +32,7 @@ def training_batched(batch_size=100):
     return load_from_csv_batched("training", batch_size)
 
 
-def test_batched(batch_size=100):
+def test_batched(batch_size=50000):
     """
     The batch size is an int representing the size of each batch.
 
@@ -55,7 +57,7 @@ def load_from_csv(datasetName):
     return data.collect().to_numpy(order="c"), labels.collect().to_numpy(order="c")
 
 
-def load_from_csv_batched(datasetName, batch_size=100):
+def load_from_csv_batched(datasetName, batch_size=50000):
     """
     The dataset name is either "training" or "test".
     The batch size is an int representing the size of each batch.
@@ -64,11 +66,28 @@ def load_from_csv_batched(datasetName, batch_size=100):
     X is a NumPy array with rows being a flattened image (row major), and columns a pixel
     y is the associated label. The label is an id that is associated with a specific character.
     """
-    data = pl.read_csv_batched(
-        f"./{datasetName}_data.csv", has_header=False, batch_size=batch_size)
-    labels = pl.read_csv_batched(
-        f"./{datasetName}_labels.csv", has_header=False, batch_size=batch_size)
 
-    while (data_batch := data.next_batches(1)) and (label_batch := labels.next_batches(1)):
-        for X, y in zip(data_batch, label_batch):
-            yield X.to_numpy(order="c"), y.to_numpy(order="c")
+    start = 0
+    while True:
+        # TODO: Find better way to handle breaking.
+        try:
+            data = pl.scan_csv(
+                f"./{datasetName}_data.csv", has_header=False, skip_rows_after_header=start, n_rows=batch_size)
+            labels = pl.scan_csv(
+                f"./{datasetName}_labels.csv", has_header=False, skip_rows_after_header=start, n_rows=batch_size)
+            start += batch_size
+            yield data.collect().to_numpy(order="c"), labels.collect().to_numpy(order="c")
+        except:
+            break
+
+
+# Keys from 0 to 62, values from 0 to 9, A to Z and a to z
+_label_to_char_dict = {label: chr(ascii) for label, ascii in enumerate(itertools.chain(
+    range(48, 58), range(65, 91), range(97, 123)))}
+
+
+def label_to_char(label):
+    """
+    Converts the given numerical label into an ascii character.
+    """
+    return _label_to_char_dict[label]
